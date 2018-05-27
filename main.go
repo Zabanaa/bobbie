4bf434914fe16ee8ea5c61be4f057a1d8881e691
karim cheurfi <karim.cheurfi@link-value.fr>
2018-05-27 13:43:31 +0100

    basic boltdb config

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
-import "bobbie/cmd"
+import (
+    "fmt"
+    "log"
+    "path/filepath"
+    "bobbie/cmd"
+    "bobbie/db"
+    homedir "github.com/mitchellh/go-homedir"
+)
+
+func check(err error) {
+
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+}
 
 func main() {
-	cmd.Execute()
+
+    homeDir, _ := homedir.Dir()
+    dbPath := filepath.Join(homeDir, "bobbie.db")
+    err := db.Init(dbPath)
+
+    check(err)
+
+    fmt.Println("DB connection established.")
+
+    cmd.Execute()
 }
